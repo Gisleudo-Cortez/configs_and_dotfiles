62763b68c54222cde856d84e7e75fcbee580469c
Gisleudo Cortez <gisleudo.cmf@gmail.com>
2025-06-07 15:53:36 -0300

    Add HyDE theme specific configs, add new binds

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,7 +21,7 @@ set -U __done_min_cmd_duration 10000
 set -U __done_notification_urgency_level low
 
 ## Environment setup
-
+source ~/.config/fish/hyde_config.fish
 # Apply .profile: use this to put fish compatible .profile stuff in
 if test -f ~/.fish_profile
   source ~/.fish_profile
