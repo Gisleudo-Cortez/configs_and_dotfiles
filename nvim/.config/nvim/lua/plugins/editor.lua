4139bafcfebe0d20d4d9ab83832aa52c8c7c5c87
Gisleudo-cortez <gisleudo.cmf@gmail.com>
2025-09-06 10:12:16 -0300

    feat(editor): add vim-matchup for enhanced block matching
    - Integrated andymass/vim-matchup with lazy-loading
    - Enabled popup display for offscreen matching delimiters
    - Activated Treesitter integration via nvim-treesitter
    - Improves navigation and visibility of matching blocks like {}, (), []

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,84 +1,91 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      ensure_installed = {
-        "bash", "c", "css", "html", "javascript", "json", "lua", "python",
-        "rust", "go", "typescript", "vim", "yaml", "sql", "markdown", "java",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {}, -- use default settings
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {}, -- use default settings
-  },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function()
-      local wk = require("which-key")
-      wk.setup({})
-      wk.add({
-        { "<leader>b", group = "Buffer", remap = false },
-        { "<leader>f", group = "Find", remap = false },
-        { "<leader>t", group = "Terminal", remap = false },
-        { "<leader>w", desc = "Save file" },
-        { "<leader>wq", desc = "Save & quit" },
-        { "<leader>bd", desc = "Delete buffer" },
-        { "<leader>ff", desc = "Find Files" },
-        { "<leader>fg", desc = "Live Grep" },
-        { "<leader>fb", desc = "Find Buffers" },
-        { "<leader>tt", desc = "Toggle terminal" },
-      }, { mode = "n" })
-    end,
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    keys = {
-      { "<leader>tt", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
-    },
-    opts = {
-      size = 15,
-      open_mapping = nil,
-      shade_terminals = true,
-      direction = "horizontal",
-    },
-  },
-
-  -- ⬇️ NEW: Indent scope highlighting
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      indent = {
-        char = "│",
-      },
-      scope = {
-        enabled = true,
-        show_start = true,
-        show_end = true,
-      },
-      exclude = {
-        filetypes = {
-          "help",
-          "dashboard",
-          "lazy",
-          "mason",
-          "nvimtree",
-          "terminal",
-        },
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			ensure_installed = {
+				"bash", "c", "css", "html", "javascript", "json", "lua", "python",
+				"rust", "go", "typescript", "vim", "yaml", "sql", "markdown", "java",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+			matchup = { enable = true }, -- 🔄 Enable integration with vim-matchup
+		},
+	},
+	{
+		"andymass/vim-matchup", -- 🔄 Highlight matching blocks/delimiters
+		event = { "BufReadPost", "BufNewFile" },
+		init = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" } -- show offscreen matches in popup
+			vim.g.matchup_override_vimtex = 1
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {}, -- use default settings
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- use default settings
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function()
+			local wk = require("which-key")
+			wk.setup({})
+			wk.add({
+				{ "<leader>b",  group = "Buffer",        remap = false },
+				{ "<leader>f",  group = "Find",          remap = false },
+				{ "<leader>t",  group = "Terminal",      remap = false },
+				{ "<leader>w",  desc = "Save file" },
+				{ "<leader>wq", desc = "Save & quit" },
+				{ "<leader>bd", desc = "Delete buffer" },
+				{ "<leader>ff", desc = "Find Files" },
+				{ "<leader>fg", desc = "Live Grep" },
+				{ "<leader>fb", desc = "Find Buffers" },
+				{ "<leader>tt", desc = "Toggle terminal" },
+			}, { mode = "n" })
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			{ "<leader>tt", "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
+		},
+		opts = {
+			size = 15,
+			open_mapping = nil,
+			shade_terminals = true,
+			direction = "horizontal",
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim", -- 📏 Indent and scope guides
+		main = "ibl",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			indent = {
+				char = "│",
+			},
+			scope = {
+				enabled = true,
+				show_start = true,
+				show_end = true,
+			},
+			exclude = {
+				filetypes = {
+					"help",
+					"dashboard",
+					"lazy",
+					"mason",
+					"nvimtree",
+					"terminal",
+				},
+			},
+		},
+	},
 }
