f4d38ea9ec851de1994060bac6de64fcd10c900c
Gisleudo-Cortez <gisleudo.cmf@gmail.com>
2026-07-14 08:38:24 -0300

    fix: use --icons=always in eza aliases to prevent arg parsing collision
    Bare --icons flag absorbed directory arguments as its optional value,
    causing 'ls <dir>' to fail with 'invalid value for --icons'. Changed all
    six eza aliases (ls, lsz, la, ll, lt, l.) to use --icons=always.

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,12 +1,12 @@
 ## Useful aliases
 
 # Replace ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
-alias lsz 'eza -al --color=always --total-size --group-directories-first --icons' # include file size
-alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons' # long format
-alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+alias ls 'eza -al --color=always --group-directories-first --icons=always' # preferred listing
+alias lsz 'eza -al --color=always --total-size --group-directories-first --icons=always' # include file size
+alias la 'eza -a --color=always --group-directories-first --icons=always' # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons=always' # long format
+alias lt 'eza -aT --color=always --group-directories-first --icons=always' # tree listing
+alias l. 'eza -ald --color=always --group-directories-first --icons=always .*' # show only dotfiles
 
 # Replace some more things with better alternatives
 alias cat 'bat --style header --style snip --style changes --style header'
